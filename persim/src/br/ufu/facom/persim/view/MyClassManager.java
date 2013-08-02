ca0f523e0a8ce2762c741cdcfe1a900dea8f1819
viniciusfm <viniciusmacielf@gmail.com>
2013-08-02 13:00:55 -0300

    Retirando bibliotecas inuteis...

--- persim/src/br/ufu/facom/persim/view/MyClassManager.java
+++ persim/src/br/ufu/facom/persim/view/MyClassManager.java
@@ -1,9 +1,6 @@
 package br.ufu.facom.persim.view;
 
 import br.ufu.facom.persim.dao.ConnectionSQLiteDAO;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class MyClassManager {
 
