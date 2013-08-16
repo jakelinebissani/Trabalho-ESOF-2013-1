70f0525bccbb023c505dc1ff1b4a134f95a98442
viniciusfm <viniciusmacielf@gmail.com>
2013-08-16 03:25:03 -0300

    Con+disconecta DB e novas Excecoes

--- persim/src/br/ufu/facom/persim/control/DisciplinaControl.java
+++ persim/src/br/ufu/facom/persim/control/DisciplinaControl.java
@@ -3,25 +3,39 @@ package br.ufu.facom.persim.control;
 import br.ufu.facom.persim.dao.ConnectionSQLiteDAO;
 import br.ufu.facom.persim.dao.DisciplinaDAO;
 import br.ufu.facom.persim.model.Disciplina;
+import java.io.FileNotFoundException;
 import java.sql.SQLException;
 
 public class DisciplinaControl {
     
     public static void save (Disciplina disc){
         try {
+            DisciplinaDAO dao = new DisciplinaDAO();
             ConnectionSQLiteDAO conn = new ConnectionSQLiteDAO();
-            DisciplinaDAO.save(disc, conn);
+            dao.save(disc, conn);
+            conn.closeDB();
         } catch (SQLException e) {
             System.err.println("Problema ao salvar disciplina: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println("Nao foi possivel encontrar plugin do banco de dados"+e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.err.println("Problema ao configurar banco de dados: "+e.getMessage());
         }
     }
     
     public static Disciplina load (String nome){
         try {
+            DisciplinaDAO dao = new DisciplinaDAO();
             ConnectionSQLiteDAO conn = new ConnectionSQLiteDAO();
-            return DisciplinaDAO.load(nome, conn);
+            Disciplina disciplina = dao.load(nome, conn);
+            conn.closeDB();
+            return disciplina;
         } catch (SQLException e) {
             System.err.println("Problema ao recuperar info. de disciplina: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println("Nao foi possivel encontrar plugin do banco de dados"+e.getMessage());
+        } catch (FileNotFoundException e) {
+            System.err.println("Problema ao configurar banco de dados: "+e.getMessage());
         }
         return null;
     }
