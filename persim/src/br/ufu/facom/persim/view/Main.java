b399ac79cd67410fafc4dd16b0788cbf647cba15
viniciusfm <viniciusmacielf@gmail.com>
2013-08-16 03:31:13 -0300

    Testes novos

--- persim/src/br/ufu/facom/persim/view/Main.java
+++ persim/src/br/ufu/facom/persim/view/Main.java
@@ -2,16 +2,21 @@ package br.ufu.facom.persim.view;
 
 import br.ufu.facom.persim.control.DisciplinaControl;
 import br.ufu.facom.persim.model.Disciplina;
+import br.ufu.facom.persim.model.Professor;
 
 public class Main {
 
     public static void main(String[] args) {
         
-        Disciplina dc = new Disciplina("ESOF", "Engenharia de Software");
+        Professor prof = new Professor("Fulano", "fulano@ufu.br", "1B123");
+        Disciplina dc = new Disciplina("ESOF", "Engenharia de Software", prof, "adicionais aqui");
         DisciplinaControl.save(dc);
         Disciplina dc2 = DisciplinaControl.load("ESOF");
-        
-        System.out.println("TESTES!!");
-        System.out.println("* "+dc2.getID()+" - "+dc2.getNome());
+        System.out.println("\nTESTES!!\n");
+        System.out.println("ID: "+dc2.getID()+"\nNome Disc: "+dc2.getNome()
+                +"\nProfessor: "+dc2.getProfessor().getNome()+"\nEmail Prof: "
+                + dc2.getProfessor().getEmail() + "\nSala Prof: "+dc2.getProfessor().getSala()
+                + "\nAdicionais: " +  dc2.getAdicionais()
+                );
     }
 }
