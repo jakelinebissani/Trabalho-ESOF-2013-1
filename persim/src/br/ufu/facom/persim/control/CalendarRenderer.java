e3bc1e2cd273f5e01fe8d16254f264d752cd2b7c
viniciusfm <viniciusmacielf@gmail.com>
2013-09-05 01:34:40 -0300

    Testes com calendario

---
+++ persim/src/br/ufu/facom/persim/control/CalendarRenderer.java
@@ -0,0 +1,72 @@
+package br.ufu.facom.persim.control;
+
+import com.javaswingcomponents.calendar.JSCCalendar;
+import com.javaswingcomponents.calendar.cellrenderers.CalendarCellRenderer;
+import com.javaswingcomponents.calendar.cellrenderers.CellRendererComponentParameter;
+import java.awt.Color;
+import java.util.Date;
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+/*
+ * Alteracoes sobre o formato e cor do calendario sao controladas por esta classe
+ */
+
+public class CalendarRenderer extends JLabel implements CalendarCellRenderer {
+
+    @Override
+    public JComponent getCellRendererComponent(CellRendererComponentParameter parameterObject) {
+        boolean isToday = parameterObject.isToday;
+        boolean isSelected = parameterObject.isSelected;
+        boolean isSelectable = parameterObject.isAllowSelection();
+        boolean hasKeyboardFocus = parameterObject.isHasFocus();
+        boolean isCurrentMonth = parameterObject.isCurrentMonth;
+        
+        String text = parameterObject.getText();
+        Date date = parameterObject.getDate();
+        JSCCalendar calendar = parameterObject.getCalendar();
+
+        setHorizontalAlignment(JLabel.CENTER);
+        setText(text);
+        setOpaque(false);
+        
+        if (isSelectable) {
+            setForeground(Color.BLACK);
+        } else {
+            setText(text);
+            setForeground(Color.LIGHT_GRAY);
+        }
+        
+        if (!isCurrentMonth) {
+            setText("");
+        }
+        
+        if (isSelected) {
+            setBorder(BorderFactory.createDashedBorder(Color.BLACK, 3, 2));
+        } else {
+            setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        }
+        
+        if (isToday && isCurrentMonth) {
+            setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        }
+
+        if (hasKeyboardFocus) {
+            setBorder(BorderFactory.createDashedBorder(Color.BLACK, 6, 2));
+            setOpaque(true);
+        }
+        return this;
+
+    }
+
+    @Override
+    public JComponent getHeadingCellRendererComponent(JSCCalendar calendar, String text) {
+        setHorizontalAlignment(JLabel.CENTER);
+        setText(text);
+        setOpaque(false);
+        setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+        setForeground(Color.BLACK);
+        return this;
+    }
+}
---
+++ persim/src/br/ufu/facom/persim/view/MainWindowFrame.java
@@ -0,0 +1,142 @@
+package br.ufu.facom.persim.view;
+
+import br.ufu.facom.persim.control.CalendarRenderer;
+import com.javaswingcomponents.calendar.JSCCalendar;
+import com.javaswingcomponents.calendar.plaf.darksteel.DarkSteelCalendarUI;
+import com.javaswingcomponents.calendar.plaf.darksteel.DarkSteelCellPanel;
+import com.javaswingcomponents.calendar.plaf.darksteel.DarkSteelCellPanelBackgroundPainter;
+import com.javaswingcomponents.calendar.plaf.darksteel.DarkSteelMonthAndYearPanel;
+import java.awt.Color;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public final class MainWindowFrame extends javax.swing.JFrame {
+    
+    public MainWindowFrame() {
+        initComponents();
+        this.setTitle("PERSIM - Personal Student Info. Manager");
+        this.createCalendar();
+        this.visibilityConfigurations();
+    }
+    
+    private void createCalendar(){
+        TimeZone timeZone = TimeZone.getDefault();
+        Locale locale = Locale.getDefault();
+        
+        this.cal = new JSCCalendar(timeZone, locale);
+        this.cal.setUI(new DarkSteelCalendarUI());
+        this.cal.setBounds(20, 20, 400, 250);
+        
+        DarkSteelCalendarUI calendarUI = (DarkSteelCalendarUI) this.cal.getUI();
+        DarkSteelMonthAndYearPanel monthAndYearPanel = (DarkSteelMonthAndYearPanel) calendarUI.getMonthAndYearPanel();
+        DarkSteelCellPanel cellPanel = (DarkSteelCellPanel) calendarUI.getCellPanel();
+        DarkSteelCellPanelBackgroundPainter cellPanelBackgroundPainter =
+                (DarkSteelCellPanelBackgroundPainter) cellPanel.getBackgroundPainter();
+        cellPanelBackgroundPainter.setHeadingBackgroundStartGradientColor(new Color(0xB0, 0xE0, 0xE6));
+        cellPanelBackgroundPainter.setHeadingBackgroundEndGradientColor(new Color(0x46, 0x82, 0xB4));
+        
+        this.cal.setCalendarCellRenderer(new CalendarRenderer());
+        this.add(this.cal);
+        
+    }
+    
+    private void visibilityConfigurations() {
+        //this.cal.setEnabled(false);
+        this.periodosAnterioresMenu.setEnabled(false);
+        this.periodosVigentesMenu.setEnabled(false);
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuItem2 = new javax.swing.JMenuItem();
+        menuBar = new javax.swing.JMenuBar();
+        periodoMenu = new javax.swing.JMenu();
+        novoPeriodoMenuItem = new javax.swing.JMenuItem();
+        periodosAnterioresMenu = new javax.swing.JMenu();
+        periodosVigentesMenu = new javax.swing.JMenu();
+        helpMenu = new javax.swing.JMenu();
+
+        jMenuItem2.setText("jMenuItem2");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        periodoMenu.setText("Periodo");
+
+        novoPeriodoMenuItem.setText("Novo Periodo");
+        periodoMenu.add(novoPeriodoMenuItem);
+
+        periodosAnterioresMenu.setText("Periodos Anteriores");
+        periodoMenu.add(periodosAnterioresMenu);
+
+        periodosVigentesMenu.setText("Periodos Vigentes");
+        periodoMenu.add(periodosVigentesMenu);
+
+        menuBar.add(periodoMenu);
+
+        helpMenu.setText("Help");
+        menuBar.add(helpMenu);
+
+        setJMenuBar(menuBar);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 800, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 495, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainWindowFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainWindowFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainWindowFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainWindowFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainWindowFrame().setVisible(true);
+            }
+        });
+    }
+    //Atributos declarados
+    private JSCCalendar cal;
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu helpMenu;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenuItem novoPeriodoMenuItem;
+    private javax.swing.JMenu periodoMenu;
+    private javax.swing.JMenu periodosAnterioresMenu;
+    private javax.swing.JMenu periodosVigentesMenu;
+    // End of variables declaration//GEN-END:variables
+}
